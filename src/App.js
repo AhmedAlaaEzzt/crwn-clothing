bfd9ab95ce42a66026b5d7240073d336d57d8826
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-07 19:54:04 +0200

    create OAuth session subscribe and unsubscribe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -7,19 +7,41 @@ import Header from "./components/header/header.component.jsx";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 import { auth } from "./firebase/firebase.utils.js";
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="shop" element={<ShopPage />} />
-          <Route path="/signin" element={<SignInAndSignUp />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  state ={
+    currentUser: null
+  }
+
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth()
+  }
+
+
+  render(){
+    return (
+      <div>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="shop" element={<ShopPage />} />
+            <Route path="/signin" element={<SignInAndSignUp />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+
 }
 
 export default App;
