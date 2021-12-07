95b8ce8b4d40a799d246d091539805baf3f8d160
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-07 19:18:33 +0200

    Move all routes from index.js to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import './App.css';
-
-
-import HomePage from './pages/homepage/homepage.component.jsx';
-
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.componet.jsx";
+import Header from "./components/header/header.component.jsx";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
+import { auth } from "./firebase/firebase.utils.js";
 
 function App() {
   return (
     <div>
-
-      <HomePage />
-      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="shop" element={<ShopPage />} />
+          <Route path="/signin" element={<SignInAndSignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,20 +4,11 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ShopPage from "./pages/shop/shop.componet.jsx";
-import Header from "./components/header/header.component.jsx";
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<App />}/>
-        <Route path="shop" element={<ShopPage />}/>
-        <Route path="/signin" element={<SignInAndSignUp />}/>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
