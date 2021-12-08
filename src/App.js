90f02fc9acf781086fe16c92a3607b088276f649
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-08 18:34:23 +0200

    install readux and store user auth data into it

--- src/App.js
+++ src/App.js
@@ -6,31 +6,27 @@ import ShopPage from "./pages/shop/shop.componet.jsx";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
-
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions.js";
 class App extends Component {
-  state = {
-    currentUser: null,
-  };
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async(userAuth) => {
-      if(userAuth){
+    const { setCurrentUser } = this.props;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser:{
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            currentUser: {
               id: snapShot.id,
-              ...snapShot.data()
-            }
-          }, ()=> console.log(this.state))
-      
-
-        })
-      }else if(isNaN(userAuth)){
-        this.setState({currentUser: userAuth},()=> console.log(this.state.currentUser))
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else if (isNaN(userAuth)) {
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -40,11 +36,10 @@ class App extends Component {
   }
 
   render() {
-    const { currentUser } = this.state;
     return (
       <div>
         <BrowserRouter>
-          <Header currentUser={currentUser} />
+          <Header />
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="shop" element={<ShopPage />} />
@@ -56,4 +51,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDespatichToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDespatichToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
-const Header = ({currentUser}) => {
+import { connect } from "react-redux";
+import { setCurrentUser } from "../../redux/user/user.actions";
+const Header = ({currentUser, setCurrentUser}) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -16,7 +18,11 @@ const Header = ({currentUser}) => {
           CONTACT
         </Link>
         {currentUser ? (
-          <div className="opetion" onClick={() => auth.signOut()}>SIGN OUT</div>
+          <Link className="opetion" to="/signin" onClick={() =>{
+             auth.signOut()
+              .then(() => setCurrentUser(null))
+              .catch(err => console.log(err))
+          }}>SIGN OUT</Link>
         ) : (
           <Link className="option" to="/signin">
             SIGNIN
@@ -27,4 +33,18 @@ const Header = ({currentUser}) => {
   );
 };
 
-export default Header;
+const mapStateToProps=(state)=>{
+  return{
+    currentUser: state.userReducer.currentUser
+  }
+}
+
+const mapDispatchToProps=(dispatch)=>{
+  return{
+    setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+  }
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)( Header);
