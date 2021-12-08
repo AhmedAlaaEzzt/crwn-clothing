fc8c06961416f0673111d8485fc0da145c555cc4
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-08 13:11:38 +0200

    fix the issue of the sign-in with google that fire submitting of the sign-in form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -56,7 +56,7 @@ class SignIn extends Component {
 
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn={true}>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn={true}>
               Sign in with Google
             </CustomButton>
           </div>
