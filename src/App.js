513ed1f8f49b422b0d1c364d7a19e9ea2cbefc26
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-08 12:45:44 +0200

    add email and password sign up feature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.componet.jsx";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
-import { auth } from "./firebase/firebase.utils.js";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
 
 class App extends Component {
   state = {
@@ -15,9 +15,23 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async(userAuth) => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, ()=> console.log(this.state))
+      
+
+        })
+      }else if(isNaN(userAuth)){
+        this.setState({currentUser: userAuth},()=> console.log(this.state.currentUser))
+      }
     });
   }
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,4 +1,5 @@
 import SignIn from '../../components/sign-in/sign-in.component.jsx';
+import SignUp from '../../components/sign-up/sign-up.component.jsx'
 
 import './sign-in-and-sign-up.styles.scss';
 
@@ -6,6 +7,7 @@ const SignInAndSignUp =()=>{
 return(
     <div className="sign-in-and-sign-up">
         <SignIn />
+        <SignUp />
     </div>
 )
 }
