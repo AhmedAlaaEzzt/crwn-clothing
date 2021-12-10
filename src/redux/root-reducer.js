e3f0759efb623ccd2f64034e993c39065a837af6
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2021-12-10 21:17:21 +0200

    create a persist store of cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,7 +2,17 @@ import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
+import {persistReducer} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
 
 
+const persistConfig = {
+    key:'root',
+    storage, 
+    whitelist:['cartReducer']
+}
 
-export default combineReducers({userReducer, cartReducer})
+const rootReducer = combineReducers({userReducer, cartReducer})
+
+export default persistReducer(persistConfig,rootReducer)
